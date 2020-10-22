c94182eac5f1faa012f06f0769acb192d0e01cbd
SaltySnail <denchikdreams@gmail.com>
2020-10-22 23:47:04 +0300

    +task2

--- spot-filters/__main__.py
+++ spot-filters/__main__.py
@@ -3,15 +3,15 @@ import time
 from tasks import *
 
 def main():
-    firstImageForPsnrTest = cv2.imread("spot-filters/images/1.jpg")
-    secondImageForPsnrTest = cv2.imread("spot-filters/images/2.jpg")
-    imageForShadeOfGrayFilter = cv2.imread("images/pic.jpg")
-    imageForConverting = cv2.imread("images/pic.jpg")
+    PsnrTestImage = cv2.imread("spot-filters/images/1.jpg")
+    PsnrTestImage2 = cv2.imread("spot-filters/images/2.jpg")
+    imageForShadeOfGrayFilter = cv2.imread("spot-filters/images/pic.jpg")
+    imageForConverting = cv2.imread("spot-filters/images/pic.jpg")
 
     result = []
 
-    result.append(task1(firstImageForPsnrTest, secondImageForPsnrTest))
-    # result.append(task2(imageForShadeOfGrayFilter))
+    result.append(task1(PsnrTestImage, PsnrTestImage2))
+    result.append(task2(imageForShadeOfGrayFilter))
     # result.append(task3(imageForConverting))
     with open("spot-filters/result/result.txt", "w") as file:
         print(result, file=file)
@@ -38,26 +38,27 @@ def task1(image1, image2):
     task_result.append(("Метрика сходства двух изображений", endTime, "psnr = " + str(psnrResult)))
     return task_result
 
-# def task_2(image):
-#     task_result = []
+def task2(image):
+    task_result = []
+    imgUMat = np.float32(image)
 
-#     startTime = time.time()
-#     averageResult = average(image)
-#     endTime = time.time() - startTime
-#     task_result.append(("Конвертация цветного изображения в монохромное изображение по формуле average", endTime, None))
-#     cv2.imwrite("result/AverageResult.jpg", averageResult)
+    startTime = time.time()
+    averageResult = average(imgUMat)
+    endTime = time.time() - startTime
+    task_result.append(("Конвертация цветного изображения в монохромное изображение по формуле average", endTime, None))
+    cv2.imwrite("spot-filters/result/AverageResult.jpg", averageResult)
     
-#     startTime = time.time()
-#     grayScaleCV = gray_scale_filter_cv(image)
-#     endTime = time.time() - startTime
-#     task_result.append(("Конвертация цветного изображения в монохромное изображение с помощью opencv", endTime, None))
-#     cv2.imwrite("result/grayScaleCVResult.jpg", grayScaleCV)
+    startTime = time.time()
+    grayScaleCV = gray_scale_filter_cv(image)
+    endTime = time.time() - startTime
+    task_result.append(("Конвертация цветного изображения в монохромное изображение с помощью opencv", endTime, None))
+    cv2.imwrite("spot-filters/result/grayScaleCVResult.jpg", grayScaleCV)
 
-#     startTime = time.time()
-#     psnrResult = psnr(averageResult, grayScaleCV)
-#     endTime = time.time() - startTime
-#     task_result.append(("Метрика сходства двух изображений", endTime, "psnr = " + str(psnrResult)))
-#     return task_result
+    startTime = time.time()
+    psnrResult = psnr(averageResult, grayScaleCV)
+    endTime = time.time() - startTime
+    task_result.append(("Метрика сходства двух изображений", endTime, "psnr = " + str(psnrResult)))
+    return task_result
 
 # def task_3(image):
 #     task_result = []
--- spot-filters/tasks/__init__.py
+++ spot-filters/tasks/__init__.py
@@ -1,3 +1,3 @@
 from tasks.task1 import *
-#from tasks.task2 import *
+from tasks.task2 import *
 #from tasks.task3 import *
